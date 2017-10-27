a592938e0b09bad889500f17c5db9d8d593a77c7
alvaro-s <alvaro.s.h.as@gmail.com>
2017-10-27 13:38:37 +0200

    Evento Pantalla (no funciona)

--- app/src/main/java/com/mastercomp/innovacion/practicainnovacion/MainActivity.java
+++ app/src/main/java/com/mastercomp/innovacion/practicainnovacion/MainActivity.java
@@ -1,13 +1,70 @@
 package com.mastercomp.innovacion.practicainnovacion;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView timer;
+    private long startTime = 0L;
+
+    private Handler customHandler = new Handler();
+
+    long timeInMS = 0L;
+    long updatedTime = 0L;
+    long timeSwapBuff = 0L;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        timer = (TextView) findViewById(R.id.timerValue);
+
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        intentFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+                    Log.i("Check","Screen went ON");
+                    timeSwapBuff += timeInMS;
+                    customHandler.removeCallbacks(updateTimer);
+                } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                    Log.i("Check","Screen went OFF");
+                    startTime = SystemClock.uptimeMillis();
+                    customHandler.postDelayed(updateTimer, 0);
+                }
+            }
+        }, intentFilter);
+
         setContentView(R.layout.activity_main);
     }
+
+    private Runnable updateTimer = new Runnable() {
+        @Override
+        public void run() {
+            timeInMS = SystemClock.uptimeMillis() - startTime;
+            updatedTime = timeSwapBuff + timeInMS;
+
+            int secs = (int) (updatedTime / 1000);
+            int mins = secs / 60;
+            secs = secs % 60;
+            int ms = (int) (updatedTime % 1000);
+            timer.setText("" + mins + ":"
+                    + String.format("%02d", secs)+ ":"
+                    + String.format("%03d", ms));
+            customHandler.postDelayed(this, 0);
+
+        }
+    };
 }
